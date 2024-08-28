8da76b254f6a63e4eb1ec4ac78cc796ee50b7f9c
Avinash <avinashchowdhury81@gmail.com>
2024-08-28 12:40:03 +0530

    refresh

--- src/middleware/roleMiddleware.ts
+++
@@ -1,7 +0,0 @@
-import { NextFunction, Request, Response } from 'express';
-
-export const roleMiddleware = (requiredRole: string) => {
-  return (req: Request, res: Response, next: NextFunction) => {
-  
-  };
-};
